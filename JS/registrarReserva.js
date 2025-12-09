28a43620748ff6175ae55e9527a036538bffd55d
RaulAndres17 <elandiii17@gmail.com>
2025-12-09 08:10:25 -0400

    Movi index a la raiz

--- JS/registrarReserva.js
+++ JS/registrarReserva.js
@@ -1,7 +1,7 @@
 document.querySelector("form").addEventListener("submit", function(e){
     e.preventDefault();
 
-    // Obtener los datos del formulario
+    // Obtenemos los datos del formulario
     const reserva = {
         nroCancha: document.querySelector('input[name="NroCancha"]').value,
         tipoCancha: document.querySelector('select[name="tipoCancha"]').value,
@@ -10,17 +10,17 @@ document.querySelector("form").addEventListener("submit", function(e){
         fecha: document.querySelector('input[name="fecha"]').value
     };
 
-    // Traer las reservas existentes del localStorage
+    // Traemos las reservas existentes
     let reservas = JSON.parse(localStorage.getItem("reservas")) || [];
 
-    // Añadir la nueva reserva
+    // Añadimos la nueva reserva
     reservas.push(reserva);
 
-    // Guardar todo de nuevo
+    // Guardamos todo de nuevo
     localStorage.setItem("reservas", JSON.stringify(reservas));
 
     alert("✅ Reserva registrada correctamente");
     
-    // Opcional: limpiar el formulario
+    // Limpiamos el formulario
     e.target.reset();
 });
